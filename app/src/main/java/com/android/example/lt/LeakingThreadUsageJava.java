4e4690edebbadd5ba9955e3340d6d2bd3ae9d91b
Dmitry <darksign@list.ru>
2022-06-21 01:50:47 +0300

    lt test classes

---
+++ app/src/main/java/com/android/example/lt/LeakingThreadUsageJava.java
@@ -0,0 +1,36 @@
+package com.android.example.lt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class LeakingThreadUsageJava extends Activity {
+    private Thread threadField;
+    private Thread threadField2;
+    private Thread threadField3;
+    private Thread threadField4 = new Thread();
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        threadField = new Thread();
+        threadField3 = new Thread();
+        threadField.start();
+        threadField2.start();
+        threadField3.start();
+        threadField4.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        threadField3.interrupt();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        // TODO clean caches or unnecessary resources
+    }
+}
